471fe4a4817f8b1775f5c3ddb9038ffe7977dda1
Hazel John <coder.twenty.25@gmail.com>
2026-09-05 21:21:35 +0530

    Day 4: connected Express app to PostgreSQL database

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 // We bring in the Express toolkit so we can build our web server
 import express from "express";
 
+// We bring in "pg" so our app can talk to our PostgreSQL database
+import pg from "pg";
+
 // We create our app - think of this as our actual website/server
 const app = express();
 
 // We choose which "door number" (port) our server listens on
 const port = 3000;
+
+// Here we set up the connection details for our database
+// Think of this like writing down the address and key to unlock our filing cabinet
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "booknotes",
+  password: "123456", // put the password you set during Postgres install
+  port: 5432,
+});
+
+// This actually opens the connection to our database
+db.connect();
 
 // This says: "when someone visits the homepage (/), send back this message"
 app.get("/", (req, res) => {
